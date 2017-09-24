8b8d31fdd9226acb8d50248a36928ee74172f9cf
Tonio Serna <antonelo@gmail.com>
2017-09-24 22:53:15 +0200

    require rspec helper and add security_code file & rspec file test

--- spec/security_code_spec.rb
+++ spec/security_code_spec.rb
@@ -1,1 +1,10 @@
 require 'spec_helper'
+
+describe SecurityCode do
+
+  let(:KEYPAD) { (1..9).to_a.freeze }
+
+  it 'is a 9 characters array' do
+    expect(KEYPAD).to have(9).characters
+  end
+end
