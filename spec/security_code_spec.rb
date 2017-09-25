129c96ce414b0bb64e75a3c55af7735a09fcf315
Tonio Serna <antonelo@gmail.com>
2017-09-25 14:32:38 +0200

    Fix and refactor basic TDD tests

--- spec/security_code_spec.rb
+++ spec/security_code_spec.rb
@@ -1,10 +1,13 @@
-require 'spec_helper'
+# require 'spec_helper', 'security_code' ...  *in ~\.rspec
 
 describe SecurityCode do
+  let(:keypad) { (1..9).to_a.freeze }
 
-  let(:KEYPAD) { (1..9).to_a.freeze }
+  it 'is available as described_class' do
+    expect(described_class).to eq(SecurityCode)
+  end
 
   it 'is a 9 characters array' do
-    expect(KEYPAD).to have(9).characters
+    expect(keypad.size).to eq(9)
   end
 end
