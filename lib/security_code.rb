75fca3414a31fda958d72a3aa1e08742ecb2de43
Tonio Serna <antonelo@gmail.com>
2017-09-26 01:53:26 +0200

    Add and fix #one

--- lib/security_code.rb
+++ lib/security_code.rb
@@ -1,6 +1,12 @@
-# lib/security_code.rb
-
+# :nodoc:
 class SecurityCode
   def one(movement)
+    if movement.eql?(:d)
+      4
+    elsif movement.eql?(:r)
+      2
+    else
+      1
+    end
   end
 end
--- spec/security_code_spec.rb
+++ spec/security_code_spec.rb
@@ -1,10 +1,14 @@
 # require 'spec_helper', 'security_code' ...  *in ~\.rspec
 
 describe SecurityCode do
-  let(:keypad) { (1..9).to_a.freeze }
+  let(:keypad)     { (1..9).to_a.freeze }
+  let(:move_down)  { described_class.new.one(:d) }
+  let(:move_up)    { described_class.new.one(:u) }
+  let(:move_right) { described_class.new.one(:r) }
+  let(:move_left)  { described_class.new.one(:l) }
 
   it 'is available as described_class' do
-    expect(described_class).to eq(SecurityCode)
+    expect(described_class).to eq(described_class)
   end
 
   it 'is a 9 characters array' do
@@ -12,14 +16,22 @@ describe SecurityCode do
   end
 
   describe '#one' do
-    one_movement =  described_class.new.one(:d)
-    it 'has down movement' do
-      expect(one_movement).to eq(4)
+    it 'moves down to 4' do
+      expect(move_down).to eq(4)
+    end
+
+    it 'moves right to 2' do
+      expect(move_right).to eq(2)
+    end
+
+    it 'is not moving' do
+      expect(move_up).to eq(1)
+      expect(move_left).to eq(1)
     end
   end
 
   it 'only first list_line has as starting point 5' do
-    first_list_line = [ 1, 3, 7, 9, 2, 9 ]
+    first_list_line = [1, 3, 7, 9, 2, 9]
     initial_list_line = [5] + first_list_line
     expect(initial_list_line.first).to eq(5)
   end
