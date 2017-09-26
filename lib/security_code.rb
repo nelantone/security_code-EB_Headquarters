a924b765a8b431066a337efcc9a60bc7ea2eaa98
Tonio Serna <antonelo@gmail.com>
2017-09-26 16:29:48 +0200

    Add one to nine keyboard methods via TDD

--- lib/security_code.rb
+++ lib/security_code.rb
@@ -9,4 +9,104 @@ class SecurityCode
       1
     end
   end
+
+  def two(movement)
+    case movement
+    when :d
+      5
+    when :l
+      1
+    when :r
+      3
+    else
+      2
+    end
+  end
+
+  def three(movement)
+    case movement
+    when :d
+      6
+    when :l
+      2
+    else
+      3
+    end
+  end
+
+  def four(movement)
+    case movement
+    when :d
+      7
+    when :u
+      1
+    when :r
+      5
+    else
+      4
+    end
+  end
+
+  def five(movement)
+    case movement
+    when :d
+      8
+    when :u
+      2
+    when :r
+      6
+    when :l
+      4
+    else
+      5
+    end
+  end
+
+  def six(movement)
+    case movement
+    when :d
+      9
+    when :u
+      3
+    when :l
+      5
+    else
+      6
+    end
+  end
+
+  def seven(movement)
+    case movement
+    when :u
+      4
+    when :r
+      8
+    else
+      7
+    end
+  end
+
+  def eight(movement)
+    case movement
+    when :u
+      5
+    when :r
+      9
+    when :l
+      7
+    else
+      8
+    end
+  end
+
+  def nine(movement)
+    case movement
+    when :u
+      6
+    when :l
+      8
+    else
+      9
+    end
+  end
 end
--- spec/security_code_spec.rb
+++ spec/security_code_spec.rb
@@ -3,30 +3,215 @@
 describe SecurityCode do
   let(:keypad)     { (1..9).to_a.freeze }
   let(:move_down)  { described_class.new.one(:d) }
-  let(:move_up)    { described_class.new.one(:u) }
-  let(:move_right) { described_class.new.one(:r) }
-  let(:move_left)  { described_class.new.one(:l) }
+  let(:move_up)    {  }
+  let(:move_right) {  }
+  let(:move_left)  {  }
 
   it 'is available as described_class' do
     expect(described_class).to eq(described_class)
   end
 
   it 'is a 9 characters array' do
-    expect(keypad.size).to eq(9)
+    expect(keypad.size).to eql(9)
   end
 
   describe '#one' do
+    move_down  = described_class.new.one(:d)
+    move_up    = described_class.new.one(:u)
+    move_right = described_class.new.one(:r)
+    move_left  = described_class.new.one(:l)
+
     it 'moves down to 4' do
-      expect(move_down).to eq(4)
+      expect(move_down).to eql(4)
     end
 
     it 'moves right to 2' do
-      expect(move_right).to eq(2)
+      expect(move_right).to eql(2)
     end
 
     it 'is not moving' do
-      expect(move_up).to eq(1)
-      expect(move_left).to eq(1)
+      expect(move_up).to eql(1)
+      expect(move_left).to eql(1)
+    end
+  end
+
+  describe '#two' do
+    move_down  = described_class.new.two(:d)
+    move_up    = described_class.new.two(:u)
+    move_right = described_class.new.two(:r)
+    move_left  = described_class.new.two(:l)
+
+
+    it 'moves down to 5' do
+      expect(move_down).to eql(5)
+    end
+
+    it 'moves right to 3' do
+      expect(move_right).to eql(3)
+    end
+
+    it 'moves left to 1' do
+      expect(move_left).to eql(1)
+    end
+
+    it 'is not moving' do
+      expect(move_up).to eql(2)
+    end
+  end
+
+  describe '#three' do
+    move_down  = described_class.new.three(:d)
+    move_up    = described_class.new.three(:u)
+    move_right = described_class.new.three(:r)
+    move_left  = described_class.new.three(:l)
+
+
+    it 'moves down to 6' do
+      expect(move_down).to eql(6)
+    end
+
+
+    it 'moves left to 2' do
+      expect(move_left).to eql(2)
+    end
+
+    it 'is not moving' do
+      expect(move_up).to eql(3)
+      expect(move_right).to eql(3)
+    end
+  end
+
+  describe '#four' do
+    move_down  = described_class.new.four(:d)
+    move_up    = described_class.new.four(:u)
+    move_right = described_class.new.four(:r)
+    move_left  = described_class.new.four(:l)
+
+    it 'moves down to 7' do
+      expect(move_down).to eql(7)
+    end
+
+    it 'moves right to 5' do
+      expect(move_right).to eql(5)
+    end
+
+    it 'moves up to 1' do
+      expect(move_up).to eql(1)
+    end
+
+    it 'is not moving' do
+      expect(move_left).to eql(4)
+    end
+  end
+
+  describe '#five' do
+    move_down  = described_class.new.five(:d)
+    move_up    = described_class.new.five(:u)
+    move_right = described_class.new.five(:r)
+    move_left  = described_class.new.five(:l)
+
+
+    it 'moves down to 8' do
+      expect(move_down).to eql(8)
+    end
+
+    it 'moves left to 4' do
+      expect(move_left).to eql(4)
+    end
+
+    it 'moves right to 6' do
+      expect(move_right).to eql(6)
+    end
+
+    it 'moves up to 2' do
+      expect(move_up).to eql(2)
+    end
+  end
+
+  describe '#six' do
+    move_down  = described_class.new.six(:d)
+    move_up    = described_class.new.six(:u)
+    move_right = described_class.new.six(:r)
+    move_left  = described_class.new.six(:l)
+
+
+    it 'moves down to 9' do
+      expect(move_down).to eql(9)
+    end
+
+    it 'moves left to 5' do
+      expect(move_left).to eql(5)
+    end
+
+    it 'moves up to 3' do
+      expect(move_up).to eql(3)
+    end
+
+    it 'is not moving' do
+      expect(move_right).to eql(6)
+    end
+  end
+
+  describe '#seven' do
+    move_down  = described_class.new.seven(:d)
+    move_up    = described_class.new.seven(:u)
+    move_right = described_class.new.seven(:r)
+    move_left  = described_class.new.seven(:l)
+
+    it 'moves left to 8' do
+      expect(move_right).to eql(8)
+    end
+
+    it 'moves up to 4' do
+      expect(move_up).to eql(4)
+    end
+
+    it 'is not moving' do
+      expect(move_down).to eql(7)
+      expect(move_left).to eql(7)
+    end
+  end
+
+  describe '#eight' do
+    move_down  = described_class.new.eight(:d)
+    move_up    = described_class.new.eight(:u)
+    move_right = described_class.new.eight(:r)
+    move_left  = described_class.new.eight(:l)
+
+    it 'moves left to 7' do
+      expect(move_right).to eql(9)
+    end
+
+    it 'moves up to 5' do
+      expect(move_up).to eql(5)
+    end
+
+    it 'moves right to 9' do
+      expect(move_right).to eql(9)
+    end
+
+    it 'is not moving' do
+      expect(move_down).to eql(8)
+    end
+  end
+
+  describe '#nine' do
+    move_down  = described_class.new.nine(:d)
+    move_up    = described_class.new.nine(:u)
+    move_right = described_class.new.nine(:r)
+    move_left  = described_class.new.nine(:l)
+
+    it 'moves up to 6' do
+      expect(move_up).to eql(6)
+    end
+
+    it 'moves left to 8' do
+      expect(move_left).to eql(8)
+    end
+
+    it 'is not moving' do
+      expect(move_down).to eql(9)
+      expect(move_right).to eql(9)
     end
   end
 
