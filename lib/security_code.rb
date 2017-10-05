74216e7039de1f88aca341616587f7e98d71c0cd
Tonio Serna <antonelo@gmail.com>
2017-10-05 14:04:32 +0200

    Make green #digit_matcher

--- lib/security_code.rb
+++ lib/security_code.rb
@@ -111,5 +111,25 @@ class SecurityCode
   end
 
   def self.digit_matcher(number)
+    case number
+    when 1
+      :one
+    when 2
+      :two
+    when 3
+      :three
+    when 4
+      :four
+    when 5
+      :five
+    when 6
+      :six
+    when 7
+      :seven
+    when 8
+      :eight
+    when 9
+      :nine
+    end
   end
 end
