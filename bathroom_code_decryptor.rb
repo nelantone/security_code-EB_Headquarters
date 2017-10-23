ca3d3ac8b0e2d486b2e930459e983ccf35c7b0ee
Tonio Serna <antonelo@gmail.com>
2017-10-23 17:52:09 +0200

    Run code decryptor from outsite the folder

--- bathroom_code_decryptor.rb
+++ bathroom_code_decryptor.rb
@@ -1,10 +1,11 @@
 require_relative 'lib/security_code'
+movements_file_path = File.expand_path("../movements.txt", __FILE__)
 
 puts 'Welcome to security bathroom code decryptor!'
 puts 'Please copy-paste your text document inside movements.txt *jumping lines included and press enter'
 gets.chomp
 puts 'Here is your bathroom code :'
-document = File.read('movements.txt')
+document = File.read(movements_file_path)
 puts '=========='
 SecurityCode.password_decoder(document).each do |digit|
   print " #{digit} "
